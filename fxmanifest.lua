80c4cfd4a7fb18d18b5d341f0b81d717030f9af3
Sylent <94993988+Sylentofficial@users.noreply.github.com>
2024-10-14 15:38:38 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version "cerulean"
+game "gta5"
+
+author "Sylent & Mr.BMW.dev"
+description "DDJ - Service Simple Loadingscreen"
+version "docs.mrbmw.de"
+
+loadscreen "html/index.html"
+
+client_scripts {
+    "client/client.lua"
+  }
+
+files {
+    "html/index.html",
+    "html/style.css",
+    "html/script.js",
+    "html/musik.mp3",
+    "html/img/background/*.jpg",
+    "html/img/logo/logo.png"
+}
